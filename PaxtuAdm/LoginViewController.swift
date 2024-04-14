25e625bc0c944ede33c8c8a98dd202c9a44152e1
mabelKruger <mabelksouza@gmail.com>
2024-04-14 19:11:10 -0300

    test

---
+++ PaxtuAdm/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  PaxtuAdm
+//
+//  Created by Mabel Kruger de Souza on 14/04/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
---
+++ PaxtuAdm/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  PaxtuAdm
+//
+//  Created by Mabel Kruger de Souza on 14/04/24.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+    
+    lazy var animationView: LottieAnimationView = {
+        let view = LottieAnimationView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAnimation()
+        setupConstraint()
+    }
+    
+    private func setupAnimation() {
+        view.backgroundColor = .white
+        animationView.animation = LottieAnimation.named("launch_screen")
+        animationView.isHidden = false
+        animationView.loopMode = .playOnce
+        animationView.play()
+    }
+    
+    private func setupConstraint() {
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            animationView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            animationView.widthAnchor.constraint(equalToConstant: 300),
+            animationView.heightAnchor.constraint(equalToConstant: 327),
+        ])
+    }
+}
+
